29f0b908014e77e5d26d3778e326fe7b5862058c
Damien Savatier <damien.savatier@epitech.eu>
2017-10-27 18:34:44 +0200

    Correctly detecting the browser name

--- events.js
+++ events.js
@@ -1,17 +1,33 @@
+function getBrowser()
+{
+  var str = window.navigator.userAgent;
+  var i   = 0;
+  var n   = 0;
+  str = str.replace(/\([^)]*\)/ig, '');
+  str = str.replace(/\)/ig, '');
+  var arr = str.split(" ");
+  while (i < arr.length) {
+    if (arr[i] == '')
+      i += 1;
+    if (n == 2)
+      return (arr[i].split('/')[0]);
+    i += 1;
+    n += 1;
+  };
+}
+
 document.getElementById("arrowGoUp").onclick = function() {
   this.className = "bounce";
   setTimeout(function() {
-    try {
+    if (getBrowser().match(/Chrome|Firefox/)) {
       window.scroll({
         top: 0,
-        left: 0,
         behavior: 'smooth'
       });
-    } catch(e) {
-        window.scroll({
-            top: 0,
-            left: 0
-        });
+    } else {
+      window.scroll({
+        top: 0,
+      });
     }
     this.className = "";
   }, 500);
