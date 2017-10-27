646c0f9cb5dbf74ad74f507498adf0eced8e20d1
Damien Savatier <damien.savatier@epitech.eu>
2017-10-27 03:00:10 +0200

    Added browser check for smooth scrolling

--- events.js
+++ events.js
@@ -1,11 +1,19 @@
 document.getElementById("arrowGoUp").onclick = function() {
   this.className = "bounce";
   setTimeout(function() {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
+    if (window.navigator.userAgent.match(/Chrome|Gecko/)) {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+    else {
+        window.scroll({
+            top: 0,
+            left: 0
+        });
+    }
     this.className = "";
   }, 500);
 
