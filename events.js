91d4d9b9604da4dc0acb036c7fd869be96528eae
Damien Savatier <damien.savatier@epitech.eu>
2017-10-27 02:45:44 +0200

    Added bouncing effect to arrow

--- events.js
+++ events.js
@@ -1,9 +1,13 @@
 document.getElementById("arrowGoUp").onclick = function() {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
+  this.className = "bounce";
+  setTimeout(function() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+    this.className = "";
+  }, 500);
 
   return false;
 };
